ce73e478efc4dff4f62197c84a3aaf85435c85b3
Woozyzzz <woozyzzz@foxmail.com>
2020-08-05 17:02:09 +0800

    deploy

---
+++ precache-manifest.e019b3973d4e6b07852a49f692508ed0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "331d50ad459363e646eb",
+    "url": "/crumb-morney-website/css/app.b866f2ae.css"
+  },
+  {
+    "revision": "39498131104a1239b0657156025795c4",
+    "url": "/crumb-morney-website/index.html"
+  },
+  {
+    "revision": "331d50ad459363e646eb",
+    "url": "/crumb-morney-website/js/app.c6b961b2.js"
+  },
+  {
+    "revision": "da59fbd8cd8b91137735",
+    "url": "/crumb-morney-website/js/chunk-vendors.6c4f2422.js"
+  },
+  {
+    "revision": "8f1f2bf309d1a586829c3c00259ee6c1",
+    "url": "/crumb-morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/crumb-morney-website/robots.txt"
+  }
+]);
